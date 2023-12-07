fa74d2bcb33fbab738c7a3015f4bb54841297369
Lucas Melo <melolucasbpm@gmail.com>
2023-12-06 23:13:23 -0300

    Update Quarto.java
    atualizando setters

--- AV2/src/main/java/com/marechal/primeira/Quarto.java
+++ AV2/src/main/java/com/marechal/primeira/Quarto.java
@@ -26,20 +26,40 @@ public class Quarto {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getNomeQuarto() {
         return nomeQuarto;
+    }
+
+    public void setNomeQuarto(String nomeQuarto) {
+        this.nomeQuarto = nomeQuarto;
     }
 
     public int getQtdeCamas() {
         return qtdeCamas;
     }
 
+    public void setQtdeCamas(int qtdeCamas) {
+        this.qtdeCamas = qtdeCamas;
+    }
+
     public boolean isTemBanheiro() {
         return temBanheiro;
     }
 
+    public void setTemBanheiro(boolean temBanheiro) {
+        this.temBanheiro = temBanheiro;
+    }
+
     public String getDescricao() {
         return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public static List<Quarto> getQuartos() {
