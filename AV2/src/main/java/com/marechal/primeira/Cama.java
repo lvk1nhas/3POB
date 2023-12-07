364458dba9afe8dfba4e3be5e20248bd66d11d3f
Lucas Melo <melolucasbpm@gmail.com>
2023-12-06 23:12:31 -0300

    Update Cama.java
    atualizando setters

--- AV2/src/main/java/com/marechal/primeira/Cama.java
+++ AV2/src/main/java/com/marechal/primeira/Cama.java
@@ -26,20 +26,40 @@ public class Cama {
         return id;
     }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
     public String getCodigoCama() {
         return codigoCama;
+    }
+
+    public void setCodigoCama(String codigoCama) {
+        this.codigoCama = codigoCama;
     }
 
     public boolean isEhBeliche() {
         return ehBeliche;
     }
 
+    public void setEhBeliche(boolean ehBeliche) {
+        this.ehBeliche = ehBeliche;
+    }
+
     public String getPosicao() {
         return posicao;
     }
 
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
     public String getDescricao() {
         return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
     }
 
     public static List<Cama> getCamas() {
